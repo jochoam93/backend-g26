1b4497870e4f607295925aae48280666003291de
Jose Ochoa <joseom41@gmail.com>
2025-08-30 13:02:38 -0500

    avance_final_semana1

--- semana01/02-colecciones-de-datos.py
+++ semana01/02-colecciones-de-datos.py
@@ -59,5 +59,79 @@ print(ejercicio_1[-2])
 
 print(ejercicio_1[6][1])
 
+#tuplas
 
+#ordenada pero no son editables
 
+#suelen ser usadas para las configuraciones de la aplicacion
+
+meses=('enero','febrero','marzo','abril')
+
+#no se pueden ni eliminar ni agregar nueva informacion a la tupla, solamente obtener informacion de la misma forma que las listas
+#del mes[1]
+# meses.append('mayo')
+
+data = ('Roxana', 'Pedro', [1,2,6,['Juan', 'Aristoteles']])
+
+#como hago para obtener aristoteles
+print(data[2][3][1])
+
+#diccionarios
+#ordenado pero por llaves no por indices y es editable
+
+persona ={
+    'nombre': 'eduardo',
+    'ciudad': 'arequipa',
+    'estado': 'peruano',
+    'nacionalidad': 'peruano',
+    'direccion':{
+        'calle': 'los girasoles',
+        'numero': 180,
+        'manzana': 3,
+        'lote': None
+    },
+    'hobbies': ['programar','estudiar','montar bici']
+
+}
+
+print(persona['nombre'])
+
+#agregar nuevos elementos al diccionario
+
+persona['idiomas'] = ('español','ingles','quechua')
+
+#editar un elemento del diccionario
+
+persona['estado']= 'soltero'
+
+#eliminar un elemento del diccionario
+
+persona.pop('nacionalidad')
+del persona['estado']
+
+print(persona)
+
+#se puede obtener las llaves del diccionario
+print(persona.keys())
+print(persona.values())
+
+#obtener la calle en la que vive la persona
+
+print(persona['direccion']['calle'])
+
+#obtener los 2 ultimos hobbies de la persona
+
+print(persona['hobbies'][-2:])
+
+#set (conjuntos)
+#similar 'al diccionario solo que no es ordenada
+
+planetas = {'tierra','marte','jupiter','urano','venus'}
+
+print(planetas)
+
+planetas.add('pluton')
+print(planetas)
+
+#tener la informacion almacenada pero solamente para corroborar si esta o no 
+print('neptuno' in planetas)
